33c59597b70cb5031f8ea9e5e0574b8ed5c53fe8
demian sims <demiansims@gmail.com>
2018-09-19 00:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 // add solution here
-function iLoveTheBeatles(number) {
-  var array = [];
-  
-  do {
-    array.push(`I love the Beatles!`);
-    number++
-    }
-  while (number < 15)
-  
-  return array;
-}
