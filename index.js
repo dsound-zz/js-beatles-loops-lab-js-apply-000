d33437ad3c627bfff4019569b5e7837981c24a9f
demian sims <demiansims@gmail.com>
2018-09-19 00:52:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-g// add solution here
-function theBeatlesPlay(musicians, instruments) {
- var array = [];
-
- for (let i=0; i < musicians.length; i++) {
- array.push(musicians[i] + " plays " + instruments[i]); }
-   
-return array  
- 
-}
-
-function johnLennonFacts(facts) {
-var array = [];
-var i = 0;
-
-  while (i < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++; }
-    
-    return array
-    
-  }
-  
+// add solution here
 function iLoveTheBeatles(number) {
   var array = [];
   
-    do {array.push("I love the Beatles!");
-    number++; }
-  while (number < 15);
-
-  return array 
+  do {
+    array.push(`I love the Beatles!`);
+    number++
+    }
+  while (number < 15)
   
-}  
+  return array;
+}
